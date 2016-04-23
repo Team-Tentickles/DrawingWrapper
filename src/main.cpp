811c0e9c31cc2d1bff1d810d950062e4c1935d82
Ben Wilcox <reccanti@gmail.com>
2016-04-23 13:56:15 -0400

    GLUT Window
    Used the GLUT Window system to bypass the max-width set by openframeworks

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include "ofMain.h"
 #include "wrapperApp.h"
+#include "ofAppGlutWindow.h"
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
+    ofAppGlutWindow window;
+	ofSetupOpenGL(&window, 3000, 1500, OF_WINDOW);			// <-------- setup the GL context
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
