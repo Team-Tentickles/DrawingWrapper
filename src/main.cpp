a4d4021d6dbca5a2fc5c79c7b33407299bb646f6
Ben Wilcox <reccanti@gmail.com>
2016-04-29 00:53:05 -0400

    Increased Width of Screen
    Made the width of the screen larger so that I could fit all of the panels

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 //========================================================================
 int main( ){
     ofAppGlutWindow window;
-	ofSetupOpenGL(&window, 4000, 1500, OF_WINDOW);			// <-------- setup the GL context
+	ofSetupOpenGL(&window, 4700, 1500, OF_WINDOW);			// <-------- setup the GL context
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
